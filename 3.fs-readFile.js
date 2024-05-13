432fac88d454cfdb4fe65cf872b6d111018e171d
Daniel Vera <110646386+DanielVeraAcero@users.noreply.github.com>
2024-05-13 03:20:38 +0000

    reading file asynchronously

---
+++ 3.fs-readFile.js
@@ -0,0 +1,14 @@
+const fs = require("node:fs");
+const { text } = require("stream/consumers");
+
+console.log("Reading first file...");
+fs.readFile("./file.txt", "utf-8", (err, text) => {
+  console.log(`Firt Text: ${text}`);
+});
+
+console.log("-----> Do things while reading the file");
+
+console.log("Reading second file...");
+fs.readFile("./file2.txt", "utf-8", (err, text) => {
+  console.log(`Second Text: ${text}`);
+});
